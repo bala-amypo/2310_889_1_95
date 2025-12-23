45492da59bcf41b430951365763382d1ac46b9b0
bala-amypo <bala@amypo.in>
2025-12-23 06:32:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.BudgetPlan;
+import com.example.demo.model.User;
 import com.example.demo.repository.BudgetPlanRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.BudgetPlanService;
 import org.springframework.stereotype.Service;
 
@@ -9,20 +11,25 @@ import org.springframework.stereotype.Service;
 public class BudgetPlanServiceImpl implements BudgetPlanService {
 
     private final BudgetPlanRepository repo;
+    private final UserRepository userRepo;
 
-    public BudgetPlanServiceImpl(BudgetPlanRepository repo) {
+    public BudgetPlanServiceImpl(BudgetPlanRepository repo,
+                                 UserRepository userRepo) {
         this.repo = repo;
+        this.userRepo = userRepo;
     }
 
     @Override
     public BudgetPlan create(Long userId, BudgetPlan plan) {
-        plan.setUserId(userId);
+        User user = userRepo.findById(userId).orElse(null);
+        plan.setUser(user);   // ✅ correct
         return repo.save(plan);
     }
 
     @Override
     public BudgetPlan get(Long userId, Integer month, Integer year) {
-        return repo.findByUserIdAndMonthAndYear(userId, month, year)
+        return repo
+                .findByUserIdAndMonthAndYear(userId, month, year)
                 .orElse(null);
     }
 }
