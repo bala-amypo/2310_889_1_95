5b7c7428634f31cf1d1ae982c84d0e5c040187d9
bala-amypo <bala@amypo.in>
2025-12-23 04:44:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,19 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String name;
-    private String type; // income or expense
+
+    private String type; // INCOME / EXPENSE
 
     public Category() {}
-    public Category(Long id, String name, String type) { this.id = id; this.name = name; this.type = type; }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
 }
